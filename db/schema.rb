72337e3c1905be0628066fdc47ac728e297d852f
aussieyang <yang.j.li91@gmail.com>
2016-03-01 14:15:38 +1100

    added score increment column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301025357) do
+ActiveRecord::Schema.define(version: 20160301031306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160301025357) do
     t.boolean  "q6?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "score"
   end
 
 end
