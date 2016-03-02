070e15b887fc70d19e126cf366db63a7d2f615fa
aussieyang <yang.j.li91@gmail.com>
2016-03-02 16:05:22 +1100

    added logout - renamed to session

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,28 +1,31 @@
-class DashboardController < ApplicationController
+class SessionController < ApplicationController
 
   def logged_in?
     !!current_user
   end
   helper_method :logged_in?
 
+
   def current_user
     User.find_by(id: session[:user_id])
   end
   helper_method :current_user
 
+
   def index
     # go to the index page that lists the quizzes
-    
   end
+
 
   def speaker
     # @user = User.find(params[:name])
-    
   end
+
 
   def login
     # takes to login form
   end
+
 
   def create
     @user = User.find_by(name: params[:name])
@@ -36,16 +39,21 @@ class DashboardController < ApplicationController
 
       session[:user_id] = new_user.id
       redirect_to '/'
-    
+
     elsif @user.speaker? == true
       session[:user_id] = @user.id
       redirect_to "/speaker/#{ @user.id }"
-      
+
     elsif @user.speaker? == false
       session[:user_id] = @user.id
       redirect_to '/'
     end
+  end
 
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get '/' => 'dashboard#index'
-  get '/login' => 'dashboard#login'
-  get '/speaker/:id' => 'dashboard#speaker'
-
-  post '/new' => 'dashboard#create'
+  get '/' => 'session#index'
+  get '/login' => 'session#login'
+  get '/speaker/:id' => 'session#speaker'
+  post '/new' => 'session#create'
+  get '/logout' => 'session#destroy'
 
   get '/api/questions/:id' => 'api/questions#quiz'
 
